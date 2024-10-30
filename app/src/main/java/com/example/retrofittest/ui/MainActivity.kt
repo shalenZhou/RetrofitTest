5e1c1865855f4f807140e6bfb455f32b9092257c
shouhuan.zhou <shouhuan.zhou@dreamsmart.com>
2024-10-30 18:16:46 +0800

    refactor Retrofit Test project

--- app/src/main/java/com/example/retrofittest/ui/MainActivity.kt
+++ app/src/main/java/com/example/retrofittest/ui/MainActivity.kt
@@ -6,11 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.retrofittest.data.App
 import com.example.retrofittest.databinding.ActivityMainBinding
 import com.example.retrofittest.service.AppService
+import com.example.retrofittest.util.ServiceCreator
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 private const val TAG = "MainActivity"
 
@@ -22,11 +21,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.getAppDataBtn.setOnClickListener {
-            val retrofit = Retrofit.Builder()
-                .baseUrl("http://10.0.2.2/") // 用于指定所有Retroﬁt请求的根路径
-                .addConverterFactory(GsonConverterFactory.create()) // 用于指定Retroﬁt在解析数据时所使用的转换库，这里指定成GsonConverterFactory
-                .build()
-            val appService = retrofit.create(AppService::class.java)
+            val appService = ServiceCreator.create<AppService>()
             appService.getAppData().enqueue(object : Callback<List<App>> {
                 override fun onResponse(call: Call<List<App>>, response: Response<List<App>>) {
                     val list = response.body()
